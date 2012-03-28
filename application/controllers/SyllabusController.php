447416821beb08d306d4f9a2a0021b6872f40cce
Joseph Workman <joeworkmn@gmail.com>
2012-03-27 20:05:11 -1000

    Syllabus view working

--- application/controllers/SyllabusController.php
+++ application/controllers/SyllabusController.php
@@ -10,7 +10,7 @@ class SyllabusController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
+        
     }
 
     public function listallAction()
@@ -20,8 +20,50 @@ class SyllabusController extends Zend_Controller_Action
        $this->view->courses = $courses;
     }
 
+    public function viewAction()
+    {
+       $coopSess = new Zend_Session_Namespace('coop');
+       $link = My_DbLink::connect();
+       if ($this->_request->isGet()) {
+                   
+          // Cast the value of 'id' to an int. Returns zero if not an int.
+          $id = (int)$this->_request->getQuery('id');
+             
+          // If user is not a student and id is empty, redirect to the listall 
+          // page so a proper course can be chosen.
+          if ($coopSess->role != 'normal' && empty($id)) {
+             $this->_helper->redirector('listall');
+          }
+                      
+          // If user is a student with a normal role, overwrite the $id variable with
+          // the course id stored in their record so they can only view the 
+          // syllabus for their course, and not get to other ones through the url.
+          if ($coopSess->role == 'normal') {
+             $uuid = $coopSess->uhinfo['uhuuid'];
+             $id = (int)$link->fetchOne("SELECT courses_id FROM coop_users 
+                                    WHERE uuid = '$uuid'");
+          }
+          
+          $course = $link->fetchRow("SELECT id, name, syllabus FROM coop_courses 
+                                     WHERE id = $id");
+      
+          if ($course) {
+             $this->view->course = $course;
+          } else {
+             $this->view->noCourse = true;
+          }
+       } 
+    }
+    
+    private function getSession()
+    {
+       return new Zend_Session_Namespace('coop');
+    }
+
 
 }
+
+
 
 
 
