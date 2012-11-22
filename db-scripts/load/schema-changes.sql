a2fb5368a1448b564acc066bd5a66f41f1a4a5ac
Joseph Workman <joeworkmn@gmail.com>
2012-11-22 13:24:55 -1000

    added schema changes to schema-changes.sql

---
+++ db-scripts/load/schema-changes.sql
@@ -0,0 +1,29 @@
+-- drop table if exists coop_jobsites;
+-- create table coop_jobsites(
+--     id INT NOT NULL AUTO_INCREMENT,
+--     position TEXT,
+--     company TEXT,
+--     hrs_per_week TEXT,
+--     semester_dates TEXT,
+--     supervisor TEXT,
+--     phone TEXT,
+--     username VARCHAR(100),
+--     classes_id INT,
+--     semesters_id INT,
+--     PRIMARY KEY(id),
+--     FOREIGN KEY(username) REFERENCES coop_users(username),
+--     FOREIGN KEY(classes_id) REFERENCES coop_classes(id),
+--     FOREIGN KEY(semesters_id) REFERENCES coop_semesters(id)
+-- ) ENGINE InnoDB;
+
+alter table coop_assignmentanswers add column submittedassignments_id INT after assignments_id;
+alter table coop_assignmentanswers add foreign key (submittedassignments_id) references coop_submittedassignments(id) ON DELETE CASCADE;
+
+
+
+drop view if exists submittedassignment_answers_view;
+create view submittedassignment_answers_view AS
+   SELECT sa.classes_id, sa.username, sa.assignments_id, 
+      sa.semesters_id, sa.is_final, aa.answer_text, aa.static_question 
+      FROM coop_submittedassignments sa JOIN coop_assignmentanswers aa
+      ON sa.id = aa.submittedassignments_id;
