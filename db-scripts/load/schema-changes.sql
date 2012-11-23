bc8741dfc35536425d4c3741921c84d0e6327d2d
Joseph Workman <joeworkmn@gmail.com>
2012-11-22 20:14:31 -1000

    get rid of jobsites table in schema-changes.sql

--- db-scripts/load/schema-changes.sql
+++ db-scripts/load/schema-changes.sql
@@ -1,25 +1,6 @@
--- drop table if exists coop_jobsites;
--- create table coop_jobsites(
---     id INT NOT NULL AUTO_INCREMENT,
---     position TEXT,
---     company TEXT,
---     hrs_per_week TEXT,
---     semester_dates TEXT,
---     supervisor TEXT,
---     phone TEXT,
---     username VARCHAR(100),
---     classes_id INT,
---     semesters_id INT,
---     PRIMARY KEY(id),
---     FOREIGN KEY(username) REFERENCES coop_users(username),
---     FOREIGN KEY(classes_id) REFERENCES coop_classes(id),
---     FOREIGN KEY(semesters_id) REFERENCES coop_semesters(id)
--- ) ENGINE InnoDB;
-
-
 -- MAKE SURE TO EMPTY ASSIGNMENTANSWERS TABLE AFTER DOING THIS BECAUSE THE RECORDS ALREADY IN THERE ARE NOT USING submittedassignments_id YET
--- alter table coop_assignmentanswers add column submittedassignments_id INT after assignments_id;
--- alter table coop_assignmentanswers add foreign key (submittedassignments_id) references coop_submittedassignments(id) ON DELETE CASCADE;
+alter table coop_assignmentanswers add column submittedassignments_id INT after assignments_id;
+alter table coop_assignmentanswers add foreign key (submittedassignments_id) references coop_submittedassignments(id) ON DELETE CASCADE;
 
 
 
