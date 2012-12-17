3a29e9a7ffc6b5a6e238f3e6299b9d84d8361482
Joseph Workman <joeworkmn@gmail.com>
2012-12-16 21:44:02 -1000

    removed unused views from sql scripts

--- db-scripts/load/create-views.sql
+++ db-scripts/load/create-views.sql
@@ -17,25 +17,6 @@ CREATE VIEW coop_users_semesters_view AS SELECT u.*, us.semesters_id, us.classes
    LEFT JOIN coop_users AS u2 ON cl.coordinator = u2.username; 
    -- LEFT JOIN coop_users AS u2 ON us.coordinator = u2.username; 
 
--- View for student information. Having two different phone types will cause results 
--- to double, and since there can be many employmentinfo recors, there will be as many
--- rows as there are empinfo records.
-DROP VIEW IF EXISTS coop_studentinfo_view;
-CREATE VIEW coop_studentinfo_view AS SELECT u.*,
-   r.role,
-   pn.phonenumber, pn.date_mod AS phn_date_mod, 
-   pt.type AS phonetype, 
-   st.grad_date, st.semester_in_major,
-   ad.address, ad.city, ad.state, ad.zipcode, ad.date_mod AS addr_date_mod, 
-   em.current_job,em.wanted_job, em.start_date, em.end_date, em.rate_of_pay, em.job_address 
-   FROM coop_users AS u LEFT JOIN coop_addresses AS ad ON u.username = ad.username 
-   LEFT JOIN coop_phonenumbers AS pn ON u.username = pn.username 
-   LEFT JOIN coop_phonetypes AS pt on pn.phonetypes_id = pt.id 
-   LEFT JOIN coop_students AS st ON u.username = st.username
-   LEFT JOIN coop_employmentinfo AS em ON u.username = em.username
-   LEFT JOIN coop_roles AS r ON u.roles_id = r.id 
-   WHERE r.role = 'user';
-
 DROP VIEW IF EXISTS coop_survey_option_amount_view;
 CREATE VIEW coop_survey_option_amount_view AS 
    SELECT a.option_amount AS global_amount, ss.option_amount AS specific_amount, 
@@ -59,13 +40,6 @@ CREATE VIEW coop_userrole_view AS
    FROM coop_users AS u 
    LEFT JOIN coop_roles AS r
       ON u.roles_id = r.id;
-
-DROP VIEW IF EXISTS coop_coordinfo_view;
-CREATE VIEW coop_coordinfo_view AS
-   SELECT u.*, pn.phonenumber
-   FROM coop_users AS u 
-   LEFT JOIN coop_phonenumbers AS pn ON u.username = pn.username 
-   LEFT JOIN coop_phonetypes AS pt ON pn.phonetypes_id = pt.id and pt.type = 'home';
 
 DROP VIEW IF EXISTS coop_incompletes_view;
 CREATE VIEW coop_incompletes_view AS
