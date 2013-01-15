b7835c2f4b1034477f3243cff8d8e77a1aafae4b
Joseph Workman <joeworkmn@gmail.com>
2013-01-14 19:14:30 -1000

    a

--- db-scripts/load/schema-changes.sql
+++
@@ -1,3 +0,0 @@
-alter table coop_assignments change column due_date fall_due_date DATE;
-alter table coop_assignments add column spring_due_date DATE after fall_due_date;
-alter table coop_assignments add column summer_due_date DATE after spring_due_date;
