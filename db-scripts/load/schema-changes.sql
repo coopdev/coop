977125800e85119b417e98728071e49d91503b68
Joseph Workman <joeworkmn@gmail.com>
2013-03-06 17:31:36 -1000

    add to schema-changes.sql

--- db-scripts/load/schema-changes.sql
+++ db-scripts/load/schema-changes.sql
@@ -1,13 +1,2 @@
-drop table if exists coop_comments;
-create table coop_comments(
-   id INT NOT NULL AUTO_INCREMENT,
-   coordinator VARCHAR(100),
-   student VARCHAR(100),
-   semesters_id INT,
-   comment TEXT,
-   date    DATETIME,
-   PRIMARY KEY(id),
-   FOREIGN KEY(coordinator) REFERENCES coop_users(username),
-   FOREIGN KEY(student) REFERENCES coop_users(username),
-   FOREIGN KEY(semesters_id) REFERENCES coop_semesters(id)
-) Engine InnoDB;
+-- alter table coop_assignments add column allowed_submit_count INT;
+update coop_assignments set allowed_submit_count = 3 where id = 4;
