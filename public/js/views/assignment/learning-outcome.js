39de5dd3ecf95faf3c15ec5cbf6ef21eb5602e55
Joseph Workman <joeworkmn@gmail.com>
2013-03-08 02:14:03 -1000

    fixed char count for new lines in TinyMCE

--- public/js/views/assignment/learning-outcome.js
+++ public/js/views/assignment/learning-outcome.js
@@ -1,28 +1,2 @@
 $(document).ready(function(){
-    // To show the user how many chars left each time they
-    // enter text into the text area.
-    $('.report').live('keyup paste drop', function(e) { 
-        chars = $(this).val();
-        totalChars = chars.length;
-        newLines = chars.match(/\r|\n/g); // searches for "\r" and "\n" chars.
-        newLines = newLines ? newLines.length : 0;
-           
-        totalChars = totalChars - newLines;
-        alert(totalChars);
-   
-        //id = $(this).attr('id');
-
-        //minLen = $('#minLen-' + id).text();
-        //charsLeft = minLen - totalChars;
-        //if (charsLeft < 0) {
-        //   charsLeft = 0;
-        //}
-        //
-        //$('#charsLeft-' + id).text(charsLeft);
-                
-    });
-
-    // When page loads, trigger the key up event on .answerText so that chars left will show up
-    // accurately after page loads from validation errors.
-    //$('.report').keyup();
 });
