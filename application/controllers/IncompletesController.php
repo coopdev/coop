e9f05784279af766e96008e26160ee7cff1b12c4
Joseph Workman <joeworkmn@gmail.com>
2013-07-16 17:55:13 -1000

    able to mark multiple students as 'Incomplete' at once

--- application/controllers/IncompletesController.php
+++ application/controllers/IncompletesController.php
@@ -15,7 +15,7 @@ class IncompletesController extends Zend_Controller_Action
        $this->view->result = $Inc->fetchAll();
 
        if ($this->getRequest()->isPost()) {
-          $incompletes = $_POST['incompletes'];
+          $incompletes = $_POST['students'];
 
           if (empty($incompletes)) {
              $this->view->resultMessage = "<p class='error'> No students were selected. </p>";
@@ -23,13 +23,41 @@ class IncompletesController extends Zend_Controller_Action
           }
 
           $Inc = new My_Model_Incompletes();
-          $Inc->removeMultipleIncompleteSatuses($incompletes);
+          $Inc->setMultipleIncompleteSatuses($incompletes, false);
           $this->view->resultMessage = "<p class='success'> Incomplete status removed on selected students. </p>";
 
           $this->view->result = $Inc->fetchAll();
        }
 
        //die(var_dump($result));
+    }
+
+    public function createMultipleAction()
+    {
+       $searchForm = new Application_Form_StudentRecSearch();
+       
+       $this->view->searchForm = $searchForm;
+
+       if ($this->getRequest()->isPost()) {
+          $data = $_POST;
+          if ($data['submittedForm'] === 'searchForm') {
+             unset($data['submittedForm']);
+             $Inc = new My_Model_Incompletes();
+             $result = $Inc->searchCompletes($data);
+             $this->view->students = $result;
+             $this->view->searchClassId = $data['classes_id'];
+             $this->view->searchSemId = $data['semesters_id'];
+          } elseif ($data['submittedForm'] === 'manageIncompletes') {
+             $students = $data['students'];
+             $Inc = new My_Model_Incompletes();
+             $Inc->setMultipleIncompleteSatuses($students);
+
+             $this->view->students = $Inc->searchCompletes(
+                     array('classes_id' => $data['searchClassId'], 
+                           'semesters_id' => $data['searchSemId']));
+          }
+
+       }
     }
 
 
--- library/My/Model/Incompletes.php
+++ library/My/Model/Incompletes.php
@@ -18,12 +18,9 @@ class My_Model_Incompletes
    {
       $db = new My_Db();
 
-      $sql = "SELECT us.id AS users_sem_id, u.fname, u.lname, u.username, c.name AS class, s.semester 
-               FROM coop_users_semesters AS us 
-               JOIN coop_users AS u ON us.student = u.username 
-               JOIN coop_classes AS c ON us.classes_id = c.id 
-               JOIN coop_semesters AS s ON us.semesters_id = s.id 
-               WHERE us.status = 'Incomplete' ORDER BY class, u.lname";
+      $sql = $this->buildStudentQuery();
+
+      $sql .= " WHERE us.status = 'Incomplete' ORDER BY class, u.lname";
 
       $result = $db->fetchAll($sql);
 
@@ -31,13 +28,13 @@ class My_Model_Incompletes
    }
 
 
-   public function removeMultipleIncompleteSatuses($incompletes)
+   public function setMultipleIncompleteSatuses($students, $markIncomplete=true)
    {
       $UsersSem = new My_Model_UsersSemester();
 
       $ids = "";
-      foreach ($incompletes as $i) {
-         $ids .= $i .","; 
+      foreach ($students as $std) {
+         $ids .= $std .","; 
       }
       $ids = substr($ids, 0, -1);
 
@@ -46,15 +43,49 @@ class My_Model_Incompletes
       $result = $UsersSem->fetchAll($where);
 
       foreach ($result as $r) {
-         $r->status = "";
+         $r->status = $markIncomplete ? "Incomplete" : "";
+         //if ($markIncomplete) {
+         //   $r->status = "Incomplete";
+         //} else {
+         //   $r->status = "";
+         //}
          $r->save();
       }
-
       //die(var_dump($sel->assemble()));
+   }
 
 
+   public function searchCompletes($criteria)
+   {
+      $db = new My_Db();
+
+      $sql = $this->buildStudentQuery();
+
+      $sql .= " WHERE us.status != 'Incomplete'";
+      foreach ($criteria as $col => $val) {
+         if (trim($val) !== "") {
+            $sql .= " AND $col = '$val'";
+         }
+      }
+      $sql .= " ORDER BY class, u.lname";
+
+      //die($sql);
+
+      $result = $db->fetchAll($sql);
+      return $result;
+      
+   }
 
 
+   private function buildStudentQuery()
+   {
+      $sql = "SELECT us.id AS users_sem_id, u.fname, u.lname, u.username, c.name AS class, s.semester 
+               FROM coop_users_semesters AS us 
+               JOIN coop_users AS u ON us.student = u.username 
+               JOIN coop_classes AS c ON us.classes_id = c.id 
+               JOIN coop_semesters AS s ON us.semesters_id = s.id";
+
+      return $sql;
    }
 }
 
