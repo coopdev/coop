6ddd8aa7868f0f98bf32ef8f60f43dff6339ce6b
Joseph Workman <joeworkmn@gmail.com>
2013-08-01 16:03:46 -1000

    remove force SSL temporarily

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -53,13 +53,13 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
       //$View->setHelperPath("My/View/Helper", "My_View_Helper");
    }
 
-   protected function _initForceSSL() {
-      if($_SERVER['SERVER_PORT'] != '443') {
-         header('Location: https://' . $_SERVER['HTTP_HOST'] . 
-                $_SERVER['REQUEST_URI']);
-         exit();
-      }
-   } 
+   //protected function _initForceSSL() {
+   //   if($_SERVER['SERVER_PORT'] != '443') {
+   //      header('Location: https://' . $_SERVER['HTTP_HOST'] . 
+   //             $_SERVER['REQUEST_URI']);
+   //      exit();
+   //   }
+   //} 
 
       
    protected function _initRoutes()
