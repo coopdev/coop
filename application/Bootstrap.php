a76882065c7bd211070eb215f900fb2b940a04a6
Joseph Workman <joeworkmn@gmail.com>
2013-08-01 15:29:10 -1000

    add https redirects in Bootstrap.php

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -52,6 +52,15 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
       //$View = new Zend_View();
       //$View->setHelperPath("My/View/Helper", "My_View_Helper");
    }
+
+
+   protected function _initForceSSL() {
+      if($_SERVER['SERVER_PORT'] != '443') {
+         header('Location: https://' . $_SERVER['HTTP_HOST'] . 
+                $_SERVER['REQUEST_URI']);
+         exit();
+      }
+   } 
       
    protected function _initRoutes()
    {
